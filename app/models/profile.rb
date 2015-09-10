dbed515b93ed72aa365643f42f2fd1d6ff08330c
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-09-09 23:21:24 -0400

    Adding many-to-many relationship between event and profile, Adding event model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ActiveRecord::Base
 	enum role: {app_user: 0, admin: 1}
 	belongs_to :user
-	has_many :events, dependent: :destroy
+	has_and_belongs_to_many :events, dependent: :destroy
 end
